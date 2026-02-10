09457245a07c4dd121749ecbc9d040f7f4cae286
skepttalk <kuldeepsingh861915@gmail.com>
2026-02-10 21:14:20 +0530

    chore:fs  system practice

--- Class_Object_Extends_Implemention/allUse.js
+++ Class_Object_Extends_Implemention/allUse.js
@@ -1,36 +1,32 @@
-class Person { 
-    constructor(name,age){
-         this.name = name;
-        this.age = age;
-    }
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-introduction(){
-    console.log(`person name:${this.name} and age is: ${this.age} year old`)
-}
-
+  introduction() {
+    console.log(`person name:${this.name} and age is: ${this.age} year old`);
+  }
 }
 
 class student extends Person {
+  constructor(name, age, course) {
+    super(name, age);
 
-    constructor(name,age,course){
+    this.course = course;
+  }
 
-        super(name,age)
-
-        this.course = course
-    }
-
-    study(){
-        console.log(`student name:${this.name} and age ${this.age} year old and course:${this.course}`)
-    }
+  study() {
+    console.log(
+      `student name:${this.name} and age ${this.age} year old and course:${this.course}`,
+    );
+  }
 }
 
-
-const s1 = new student("kuldeep singh ",20,"MCA")
+const s1 = new student("kuldeep singh ", 20, "MCA");
 
 s1.introduction();
-s1.study()
-
-
+s1.study();
 
 
 
