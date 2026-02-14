1c8a26c56a5d293c077b599a16618676dd98e155
skepttalk <kuldeepsingh861915@gmail.com>
2026-02-14 20:07:28 +0530

    feat:added anagram

---
+++ Logic-Practice/anagram.js
@@ -0,0 +1,24 @@
+let str = "bus";
+let str2 = "sub";
+
+if (str.length !== str2.length) {
+    console.log("not anagram");
+} else {
+    for (let i = 0; i < str.length; i++) {
+        let found = false;
+
+        for (let j = 0; j < str2.length; j++) {
+            if (str[i] === str2[j]) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            console.log("its not an anagram");
+            return;
+        }
+    }
+
+    console.log("its an anagram");
+}
