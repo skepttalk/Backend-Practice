da1424e33ab4cbaad1a9556f3a2831783407bfff
skepttalk <kuldeepsingh861915@gmail.com>
2026-02-15 17:59:29 +0530

    feat:capital first letter of word

---
+++ Logic-Practice/CapitalFirstLetter.js
@@ -0,0 +1,9 @@
+function capitalizeWords(str) {
+  let words = str.split(" ");
+
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+  }
+
+  return words.join(" ");
+}
