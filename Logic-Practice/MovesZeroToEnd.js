c829c6cacb1f470408769fc897a50d87ba3f4ec8
skepttalk <kuldeepsingh861915@gmail.com>
2026-02-17 12:30:17 +0530

    feat:in array moves zero to end new method add last method self

--- Logic-Practice/MovesZeroToEnd.js
+++ Logic-Practice/MovesZeroToEnd.js
@@ -1,14 +1,45 @@
-function moveZeros(arr) {
-    let j = 0;
+// function moveZeros(arr) {
+//     let j = 0;
 
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] !== 0) {
-            [arr[i], arr[j]] = [arr[j], arr[i]];
-            j++;
-        }
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] !== 0) {
+//             [arr[i], arr[j]] = [arr[j], arr[i]];
+//             j++;
+//         }
+//     }
+
+//     return arr;
+// }
+
+// console.log(moveZeros([0,1,0,3,12]));
+
+
+// let arr = [20, 0, 0, 43, 89, 3];
+// let i = 0;
+// let count = arr.length;
+
+// while (i < count) {
+//   if (arr[i] === 0) {
+//     arr.splice(i, 1);
+//     arr.push(0);
+//     count--;
+//   } else {
+//     i++;
+//   }
+// }
+
+// console.log(arr);
+
+
+let arr = [40,33,22,0,2,0,3,0,234]
+
+
+for(let i =0;i<arr.length;i++){
+    if(arr[i] === 0){
+        arr.splice(i,1)
+        arr.push(0);
     }
-
-    return arr;
 }
 
-console.log(moveZeros([0,1,0,3,12]));
+console.log(arr)
+
