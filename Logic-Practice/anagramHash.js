8248f4a2152e0b6df629fa620d707e852983fedf
skepttalk <kuldeepsingh861915@gmail.com>
2026-02-20 16:10:49 +0530

    feat:anagram using hashmap

---
+++ Logic-Practice/anagramHash.js
@@ -0,0 +1,24 @@
+let str = "bus";
+let nstr = "sub";
+
+if (str.length !== nstr.length) {
+    console.log("Not an anagram");
+    return;
+}
+
+let map = {};
+
+
+for (let char of str) {
+    map[char] = (map[char] || 0) + 1;
+}
+
+for (let char of nstr) {
+    if (!map[char]) {
+        console.log("Not an anagram");
+        return;
+    }
+    map[char]--;
+}
+
+console.log("It is an anagram");
