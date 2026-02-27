9cde2bb09effc22e50f0291fc1ec6151b253d04f
skepttalk <kuldeepsingh861915@gmail.com>
2026-02-27 20:09:36 +0530

    fix:fixed logic of reverse  array of two poiter

--- Logic-Practice/reverseTwoSum.js
+++ Logic-Practice/reverseTwoSum.js
@@ -1,6 +1,17 @@
 let arr = [1, 2, 3, 4, 5];
 
-let left = arr[o];
+let left = 0;
+let right = arr.length - 1;
 
-let right = arr.length-1;
+while (left < right) {
 
+    // swap
+    let temp = arr[left];
+    arr[left] = arr[right];
+    arr[right] = temp;
+
+    left++;
+    right--;
+}
+
+console.log("reverse array:", arr);
