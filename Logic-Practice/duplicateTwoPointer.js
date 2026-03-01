ed20e8893d08576ec8196d563515b7ee282951d2
skepttalk <kuldeepsingh861915@gmail.com>
2026-03-01 17:29:35 +0530

    fix:two pointer duplicate rmove

--- Logic-Practice/duplicateTwoPointer.js
+++ Logic-Practice/duplicateTwoPointer.js
@@ -1,1 +1,18 @@
+
+
+
+
 let arr = [1,1,2,2,3,4,4];
+
+let i = 0; 
+
+for (let j = 1; j < arr.length; j++) {
+
+    if (arr[j] !== arr[i]) {
+        i++;
+        arr[i] = arr[j];
+    }
+}
+
+console.log("Unique count:", i + 1);
+console.log("Modified array:", arr.slice(0, i + 1));
